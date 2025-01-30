4503b3fd0836565f85d7ac2e31ed21800b4c92e4
Dominik Haładus <dominik.haladus@protonmail.com>
2025-01-30 16:02:46 +0100

    Easymode logic

--- src/Game.java
+++ src/Game.java
@@ -1,82 +1,42 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Game {
+public class Game extends Main {
 
     // Metoda do losowania liczby w zakresie 1-100
     static int numberDraw() {
-
         Random randomNumber = new Random();
         int guessNumber = randomNumber.nextInt(101);
-        System.out.println("The number to guess has been drawn, good luck");
+        System.out.println("The number to guess has been drawn, good luck.");
         return guessNumber;
-
     }
 
     // Metoda zawierające logikę poziomu trudności Easy
-    static void easyMode() {
-
+    static void easyMode(int guessNumber) {
+        System.out.println("In this mode you have 10 attempts");
         for (int attemptsNumber = 0; attemptsNumber < 10; attemptsNumber++) {
 
             Scanner attemptScanner = new Scanner(System.in);
             System.out.println("Make a guess: ");
             int attempt = attemptScanner.nextInt();
 
-            if (attempt > numberDraw()) {
+            if (attempt > guessNumber) {
                 System.out.println("Too high.Try again.");
-            } else if (attempt < numberDraw()) {
+            } else if (attempt < guessNumber) {
                 System.out.println("Too low.Try again");
             } else {
                 System.out.println("Congratulations! You win!");
+                Game.revealNumber();
                 break;
             }
         }
-    }
-
-    // Metoda zawierające logikę poziomu trudności Medium
-    static void mediumMode() {
-
-        for (int attemptsNumber = 0; attemptsNumber < 7; attemptsNumber++) {
-
-            Scanner attemptScanner = new Scanner(System.in);
-            System.out.println("Make a guess: ");
-            int attempt = attemptScanner.nextInt();
-
-            if (attempt > numberDraw()) {
-                System.out.println("Too high.Try again.");
-            } else if (attempt < numberDraw()) {
-                System.out.println("Too low.Try again");
-            } else {
-                System.out.println("Congratulations! You win!");
-                break;
-            }
-        }
-    }
-
-    // Metoda zawierające logikę poziomu trudności Hard
-    static void hardMode() {
-
-        for (int attemptsNumber = 0; attemptsNumber < 5; attemptsNumber++) {
-
-            Scanner attemptScanner = new Scanner(System.in);
-            System.out.println("Make a guess: ");
-            int attempt = attemptScanner.nextInt();
-
-            if (attempt > numberDraw()) {
-                System.out.println("Too high.Try again.");
-            } else if (attempt < numberDraw()) {
-                System.out.println("Too low.Try again");
-            } else {
-                System.out.println("Congratulations! You win!");
-                break;
-            }
-        }
+        System.out.println("Sorry, you lost");
+        Game.revealNumber();
     }
 
     //Metoda wyświetlająca liczbę, jeśli gracz przegrał
-
     static int revealNumber() {
-        System.out.println("Wylosowana liczba to: "+numberDraw()+".");
+        System.out.println("Draw number is: "+numberDraw()+".");
         return 0;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,23 @@ public class Main {
         String userName = username.nextLine();
 
         // Zasady gry Number Guessing Game
-        System.out.println("Welcome "+userName+" in Number Guessing Game");
+        System.out.println("Welcome " + userName + " in Number Guessing Game");
         System.out.println("Let me describe rules of the game: ");
         System.out.println("1. You can choose level of difficulty (easy, medium or hard.)");
         System.out.println("2. After this the game will immediately begin.");
         System.out.println("3. Depends on level you choose you will have certain amount of attempts and time to guess a number between 1-100.");
 
-        // Sprawdzenie gotowości gracza do gry
+        Main.startGame();
+    }
+
+        static void startGame(){
         Scanner gameStart = new Scanner(System.in);
         System.out.println("Are you ready? [Yes/No]");
         String start = gameStart.nextLine();
 
         if (start.equals("Yes")) {
             System.out.println("Let's start");
+            Game.easyMode(Game.numberDraw());
         }
         else {
             System.out.println("Thank you for your time. The game will shutdown");
